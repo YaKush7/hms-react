541cb11a99a04c61462e394c92e13021ec9af837
Yashk <yashkushwaha227@gmail.com>
2020-11-02 01:04:22 +0530

    Minor Changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import Main from "./components/Main";
-import { Switch, Route, Redirect, BrowserRouter } from "react-router-dom";
+import { Switch, Route, Redirect, HashRouter } from "react-router-dom";
 import { SLogin, PLogin } from "./components/Login/Loginform";
 
 const App = () => {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route exact path="/" component={Main} />
         <Route path="/staff_login" component={SLogin} />
         <Route path="/patient_login" component={PLogin} />
         <Redirect exact to="/" />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
